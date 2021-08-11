07b556c44de397e7170613c4901dc20b6849ba4f
Nahuel Ovando <nahuel@yahoo.com.ar>
2021-08-11 10:34:56 -0300

    Prueba de clone

--- SaludoCobol.cbl
+++ SaludoCobol.cbl
@@ -2,6 +2,6 @@ IDENTIFICATION DIVISION.
 PROGRAM-ID. HELLO-WORLD.
 * Saludos para todos los dias.
 PROCEDURE DIVISION.
-    DISPLAY 'Buen dia!!! hoy es lunes'.
+    DISPLAY 'Buen dia!!! hoy es Miercoles'.
     DISPLAY 'Curso: VISUAL STUDIO'.
     STOP RUN.
